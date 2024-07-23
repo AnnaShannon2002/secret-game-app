1b80a5456db5806b2935c104349c0875db6ecf84
Anna Shannon <149623553+AnnaShannon2002@users.noreply.github.com>
2024-07-23 02:07:32 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,6 @@ $( document ).ready(function() {
     
   // iss1 click btn load games. https://github.com/AnnaShannon2002/game-app/issues/1
 
-  //what dom ?
-  // $('#btnLoadGames').click(function() {
-
-  // $('main').fadeIn(3000);
-
   $('#btnLoadGames').on('click', function() {
 
     $('#gameList').empty();
@@ -19,26 +14,7 @@ $( document ).ready(function() {
     console.log("clicked load games")
     console.log('more lines of code');
 
-    //the best way...backticks and decorators..
-    // ` backtick
-
     console.log(gameList);
-
-    // $('#gameList').append(`<li class="list-group-item"> ${ gameList[0] } </li>`);
-
-    //another way is to \'
-    // $('#gameList').append('<li>Call of Duty: Modern Warfare</li>');
-
-    // $('#gameList').append(`<li>Call of Duty: Modern Warfare</li>`);
-
-    // meh.
-    // $('#gameList').append("<li class=\"list-group-item\">" + gamesList[0] + "</li>");
-
-    //loop through the array 
-    //the traditional...for loop.
-    // for (i = 0; i < gameList.length; i++) {
-    //    $('#gameList').append(`<li class="list-group-item"> ${gameList[i]} </li>`);
-    // }
 
     $(gameList).each(function(index, value) {
 
@@ -55,20 +31,7 @@ $( document ).ready(function() {
     $('#gameList').slideToggle(1000).empty();
 
   });
-  
-
-  //dom .. load in data ..
-  //more code here
 });
-
-
-/* this is kinda what vanilla js looks like.
-var el;
-el.document.gerElementByTagName('button');
-el.addEventListener('click', ) */
-
-// make a data thingy: array (list)
-// var => let or const = 22/7
 
 let gameList = [
   "Call of Duty: Modern Warfare",
