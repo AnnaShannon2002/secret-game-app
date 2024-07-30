284b33cff87fc2d22c464467b93f6ac86935e858
Anna Shannon <149623553+AnnaShannon2002@users.noreply.github.com>
2024-07-29 23:24:39 -0500

    Add files via upload

---
+++ scripts/data.js
@@ -0,0 +1,49 @@
+data = {
+
+    games : [
+      {
+          title : "Mortal Kombat 11",
+          developer : "NetherRealm Studios",
+          singleplayer : "yes",
+          multiplayer : "yes",
+          tags : ["fighting", "action", "violent"]
+      },
+      {
+          title : "Call of Duty: Modern Warfare",
+          developer : "Infinity Ward",
+          singleplayer : "yes",
+          multiplayer : "yes",
+          tags : ["shooter", "action", "violent"]
+      },
+      {
+          title : "Batman: Arkham Knight",
+          developer : "Rocksteady Studios",
+          singleplayer : "yes",
+          multiplayer : "no",
+          tags : ["action", "adventure", "violent"]
+      },
+      {
+          title : "Red Dead Redemption 2",
+          developer : "Rockstar Games",
+          singleplayer : "yes",
+          multiplayer : "yes",
+          tags : ["action", "adventure", "violent"]
+      },
+      {
+          title : "Minecraft",
+          developer : "Mojang Studios",
+          singleplayer : "yes",
+          multiplayer : "yes",
+          tags : ["sandbox", "adventure", "creative"]
+      },
+      {
+          title : "Runescape",
+          developer : "Jagex",
+          singleplayer : "no",
+          multiplayer : "yes",
+          tags : ["mmorpg", "adventure", "creative"]
+      }
+    ]
+  
+  }
+  
--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,15 @@
 $( document ).ready(function() {
 
     console.log("the doc is ready!");
-    
-  // iss1 click btn load games. https://github.com/AnnaShannon2002/game-app/issues/1
 
+    loadGameData("gameList");
+
+    addEvents();
+    
   $('#btnLoadGames').on('click', function() {
 
     $('#gameList').empty();
 
-    //IIFE = immediately invoked function expression
     
     console.log("clicked load games")
     console.log('more lines of code');
@@ -21,8 +22,6 @@ $( document ).ready(function() {
       $('#gameList').append(`<li class="list-group-item"> ${value} </li>`)
 
     });
-
-    
   
   });
 
@@ -33,10 +32,114 @@ $( document ).ready(function() {
   });
 });
 
-let gameList = [
-  "Call of Duty: Modern Warfare",
-  "Batman: Arkham Knight", 
-  "Red Dead Redemption 2",
-  "Minecraft",
-  "Runescape"
-]
+function loadGameData(appendId) {
+
+  let appendElement = $(`#${appendId}`);
+
+  appendElement.empty();
+
+  $.each(data.games, (index, song)=>{
+    appendElement.append(`
+      <li id="gameNo${index}Name" class="list-group-item mb-1 gameName">
+      ${game.name}
+
+      <ul class"list-group gameDetails">
+        <li class="input-group w-100">
+          <span class="w-20 input-group-text">title</span>
+            <input id="gameNo${index}TitleEdit" name="title type="text" class="form-control editGame" value="${song.title}">
+        </li>
+        <li class="input-group w-100">
+          <span class="w-20 input-group-text">developer</span>
+            <input id="gameNo${index}DeveloperEdit" name="developer type="text" class="form-control editGame" value="${song.developer}">
+        </li>
+        <li class="input-group w-100">
+          <span class="w-20 input-group-text">singleplayer</span>
+            <input id="gameNo${index}SingleplayerEdit" name="singleplayer type="text" class="form-control editGame" value="${song.singleplayer}">
+        </li>
+        <li class="input-group w-100">
+          <span class="w-20 input-group-text">multiplayer</span>
+            <input id="gameNo${index}MultiplayerEdit" name="multiplayer type="text" class="form-control editGame" value="${song.multiplayer}">
+        </li>
+        <li class="input-group w-100">
+          <span class="w-20 input-group-text">tags</span>
+            <input id="gameNo${index}TagsEdit" name="tags type="text" class="form-control editGame" value="${song.tags}">
+        </li>
+      </ul>
+    </li>`);
+
+      $('.gameDetails').hide();
+      $('input.editGame').prop('readonly', true);
+
+  });
+
+}
+
+function addEvents(){
+  $('.gameName').on('click', (e)=> {
+    let $this = $(e.target);
+    let $thisId = $this.attr('id');
+
+    $('#'+ $thisId +' > ul.gameDetails').toggle();
+  });
+
+  $('#btnHideAll').on('click', (e)=> {
+    $('ul.gameDetails').hide();
+  });
+  
+  $('#btnShowAll').on('click', (e)=> {
+    $('ul.gameDetails').show();
+  });
+
+  $('input.editGame').on('click', (e)=> {
+
+    let $this = $(e.target);
+    $this.prop('readonly', false);
+
+  });
+
+  $('#btnSaveSong').on('click', ()=> {
+
+    data.games.push({
+      title : $('#gameAddTitle').val(),
+      developer : $('#gameAddDeveloper').val(),
+      singleplayer : $('#gameAddSingleplayer').val(),
+      multiplayer : $('#gameAddMultiplayer').val(),
+      tags : $('#gameAddTags').val()
+    });
+
+    loadGameData("gameList");
+    addEvents();
+
+    $('#addGameModal .btn-close').click()
+    $('#addGameModal input').val('');
+  });
+
+  $('input.editSong').on('blur', (e)=> {
+
+    let $this = $(e.target);
+    let $thisId = $this.attr('id');
+    let $thisKey = $this.attr('name');
+     console.log($this.key);
+
+    let regexDigit = /\d+/g;
+    let gameIndex = $thisId.match(regexDigit)[0];
+
+
+    data.games[gameIndex][$thisKey] = $this.val();
+
+    $(e.target).prop('readonly', true);
+
+  });
+
+  $('#btnConsoleData').on('click', ()=> {
+    console.log(data.games); 
+  });
+}
+
+// let gameList = [
+//   "Call of Duty: Modern Warfare",
+//   "Batman: Arkham Knight", 
+//   "Red Dead Redemption 2",
+//   "Minecraft",
+//   "Runescape"
+// ]
